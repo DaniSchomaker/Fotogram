3bcab79ebe15e926ad0093a628a4fcf5e01f12b7
Dani S. <daniela.schomaker@posteo.de>
2025-09-04 12:58:11 +0200

    add function getPhotoTemplate(i)

---
+++ scripts/templates.js
@@ -0,0 +1,11 @@
+// Photo-Template
+
+function getPhotoTemplate(i) {
+    return `
+        <div class="single-door">
+            <img src="${photos[i]}" alt="${photosAlt[i]}">
+        </div>
+    `;
+}
+
+
