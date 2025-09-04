64695c5e8d4c9f96b47d279a2642f5917061dfe1
Dani S. <daniela.schomaker@posteo.de>
2025-09-04 21:46:03 +0200

    add function getPhotoLightboxTemplate(i)

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,11 +1,15 @@
-// Photo-Template
-
-function getPhotoTemplate(i) {
+// Photo-Template SMALL --> Die Übersicht
+function getPhotoPreviewGalleryTemplate(i) {
     return `
-        <div class="single-door">
-            <img src="${photos[i]}" alt="${photosAlt[i]}">
+        <div class="photo_preview">
+            <img src="${photos[i]}" alt="${photosAlt[i]}" onclick="openLightbox(${i})">
         </div>
     `;
 }
 
-
+// BIG Photo-Template Lightbox
+function getPhotoLightboxTemplate(i) {
+    return `
+        <img src="${photos[i]}" alt="${photosAlt[i]}" class="photo_lightbox">
+    `;
+}
