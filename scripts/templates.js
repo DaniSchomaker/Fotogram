c6b17de03eaf1cc60564a32f1814c8e4b3b70822
Dani S. <daniela.schomaker@posteo.de>
2025-09-08 16:26:35 +0200

    feat: add aria-attributs and Focusshift for accessibility

--- scripts/templates.js
+++ scripts/templates.js
@@ -2,17 +2,23 @@
 
 function getPhotoPreviewGalleryTemplate(i) {
   return `
-    <div class="photo_preview">
-      <img src="${PHOTOS[i]}" alt="${PHOTOS_ALT[i]}" onclick="openLightbox(${i})">
-    </div>`;
+    <button class="photo_preview" onclick="openLightbox(${i})">
+      <img 
+        src="${PHOTOS[i]}" 
+        alt="${PHOTOS_ALT[i]}" 
+        class="photo_preview_img"
+       />
+    </button>`;
+
+    // die Fotos werden als Buttons eingefügt damit man sie per Tab+Enter (ohne Maus) öffnen kann
 }
 
 // Lightbox
 
 function getHeaderLightboxTemplate(i) {
   return `
-    <h2>${PHOTOS_ALT[i]}</h2>
-    <button onclick="closeLightbox()">
+    <h2 id="lightbox_title">${PHOTOS_ALT[i]}</h2>
+    <button aria-label="Fotogroßansicht schließen" onclick="closeLightbox(), setFocusOnTop()">
     <img src="./assets/icons/Icon_X_ChatGPT.png" alt="Schließen-Button" class="icon_lightbox" />
     </button>`;
 }
@@ -24,14 +30,14 @@ function getPhotoLightboxTemplate(i) {
 
 function getFooterLightboxTemplate(i) {
   return `
-    <button onclick="showPreviousPhoto()">
+    <button aria-label="Navigationspfeil: ein Foto zurück" onclick="showPreviousPhoto()">
     <img
       src="./assets/icons/Icon_Pfeil_links_ChatGPT.png"
       alt="Navigationspfeil: ein Foto zurück"
       class="icon_lightbox"/>
     </button>
     <span class="photo_number">${i + 1}/${PHOTOS.length}</span>
-    <button onclick="showNextPhoto()">
+    <button aria-label="Navigationspfeil: ein Foto weiter" onclick="showNextPhoto()">
     <img
       src="./assets/icons/Icon_Pfeil_rechts_ChatGPT.png"
       alt="Navigationspfeil: ein Foto weiter"
